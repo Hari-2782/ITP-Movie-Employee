a4a18c798758871f64754bd2b57bdce0a4c2ba3a
hari27 <hmsanth@gmail.com>
2024-05-16 17:49:58 +0530

    first commit

---
+++ src/API_URL/api_url.js
@@ -0,0 +1,19 @@
+export const API_URL = {
+    // DEPARTMENT: 'http://localhost:8000/api/adminleave/',
+     EMPLOYEE: 'http://localhost:8000/api/employee/',
+     PROFILEPHOTO: 'http://localhost:8000/api/fileupload/',
+     photosPath: `http://localhost:8000/profile/`,
+     LEAVE:  `http://localhost:8000/api/leave/`,
+     LEAVEPHOTO:'http://localhost:8000/api/leavefileupload/',
+     leavephotospath:' http://localhost:8000/profile2/',
+     ATTENDANCE:  `http://localhost:8000/api/attendance/`,
+     LEAVEAPPROVE: 'http://localhost:8000/api/leavepermit/',
+      EMAIL:'http://localhost:8000/api/email/',
+ 
+ 
+ 
+     REGISTER: 'http://localhost:8000/api/register/',
+     LOGIN: 'http://localhost:8000/api/login/',
+ 
+ }
+ 
